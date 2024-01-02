65c06aa7bce773f8e29b5c1f1eb72e7f5801cd33
taiseen <taiseen.cse@gmail.com>
2024-01-02 17:57:01 +0600

    feat: animated radial menu

---
+++ OnlineTutotial/JS/AnimatedRadialMenu/script.js
@@ -0,0 +1,6 @@
+// 2 January 2024
+
+const toggleBtn = document.querySelector('.toggleBtn');
+const navMenu = document.querySelector('.navMenu');
+
+toggleBtn.addEventListener('click', () => navMenu.classList.toggle('show'));
