b90c0f00f6fd825db4f9bd70e9e330150d2aaa5d
taiseen <taiseen.cse@gmail.com>
2024-01-14 01:09:40 +0600

    fix: get/set range slider value

--- OnlineTutotial/JS/Range-Slider-Custom-Animated/script.js
+++ OnlineTutotial/JS/Range-Slider-Custom-Animated/script.js
@@ -1,2 +1,19 @@
-// 3 January 2024
+// 14 January 2024
 
+const inputSlider = document.querySelector('input');
+const sliderValue = document.querySelector('span');
+
+inputSlider.addEventListener('input', (e) => {
+
+    const value = e.target.value;
+
+    sliderValue.textContent = value;
+
+    sliderValue.style.left = (value / 2) + '%';
+
+    sliderValue.classList.add('show');
+})
+
+inputSlider.addEventListener('blur', () => {
+    sliderValue.classList.remove('show');
+});
