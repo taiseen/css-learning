4b929acbad575461c0b95636b6209840281efd5d
taiseen <taiseen.cse@gmail.com>
2024-07-01 05:26:12 +0600

    feat: projects indexing

---
+++ assets/db.js
@@ -0,0 +1,86 @@
+const projects = [
+    {
+        name: 'Animated-Fancy-Border',
+        link: '../OnlineTutotial/CSS/Animated-Fancy-Border/index.html',
+    },
+    {
+        name: 'Keyboard-Keys-UI',
+        link: '../OnlineTutotial/CSS/Keyboard-Keys-UI/index.html',
+    },
+    {
+        name: 'Animated-Circular-Navigation-Menu',
+        link: '../OnlineTutotial/JS/Animated-Circular-Navigation-Menu/index.html',
+    },
+    {
+        name: 'Animated-Radial-Menu',
+        link: '../OnlineTutotial/JS/Animated-Radial-Menu/index.html',
+    },
+    {
+        name: 'Live Character-Word Counter',
+        link: '../OnlineTutotial/JS/Live Character-Word Counter/index.html',
+    },
+    {
+        name: 'Magnetic-Hover-Effects',
+        link: '../OnlineTutotial/JS/Magnetic-Hover-Effects/index.html',
+    },
+    {
+        name: 'Neumorphism-Custom-Range-Slider',
+        link: '../OnlineTutotial/JS/Neumorphism-Custom-Range-Slider/index.html',
+    },
+    {
+        name: 'Range-Slider-Custom-Animated',
+        link: '../OnlineTutotial/JS/Range-Slider-Custom-Animated/index.html',
+    },
+
+    {
+        name: 'SpinWheel Canvas',
+        link: './SpinWheel/codecc/index.html',
+    },
+    {
+        name: 'SpinWheel',
+        link: './SpinWheel/OnlineTutorials/index.html',
+    },
+    {
+        name: 'ProgressBar - File upload',
+        link: '../Progressbar/HoneyWebDesigner/index.html',
+    },
+    {
+        name: 'ProgressBar - File upload | http-bin',
+        link: '../Progressbar/OpenJavaScript/index.html',
+    },
+    {
+        name: 'Character Showcase',
+        link: '../CodingMonk/CSS/index.html',
+    },
+    {
+        name: 'Context Menu',
+        link: '../ContextMenu/CodingNepal/index.html',
+    },
+    {
+        name: 'Hover Effect',
+        link: '../Hyperplexed/01.HoverEffect/index.html',
+    },
+    {
+        name: 'Layout-1',
+        link: '../LayoutSystem/Flex/layout-1/index.html',
+    },
+    {
+        name: 'Layout-2',
+        link: '../LayoutSystem/Flex/layout-2/index.html',
+    },
+    {
+        name: 'Layout-3',
+        link: '../LayoutSystem/Flex/layout-3/index.html',
+    },
+    
+    // {
+    //     name: 'Animate On Scroll',
+    //     link: './SnippetsCode/AnimateOnScroll/index.html',
+    // },
+    // {
+    //     name: 'Multi Level Menu',
+    //     link: './SnippetsCode/MultiLevelMenu/index.html',
+    // },
+]
+
+export default projects;
