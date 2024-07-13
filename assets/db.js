ec7f0c14e6409ab4683debfe568d3bc4cec75d85
taiseen <taiseen.cse@gmail.com>
2024-07-14 01:05:53 +0600

    fix: project link udate

--- assets/db.js
+++ assets/db.js
@@ -1,35 +1,35 @@
 const projects = [
     {
         name: 'Animated-Fancy-Border',
-        link: '../OnlineTutotial/CSS/Animated-Fancy-Border/index.html',
+        link: './OnlineTutotial/CSS/Animated-Fancy-Border/index.html',
     },
     {
         name: 'Keyboard-Keys-UI',
-        link: '../OnlineTutotial/CSS/Keyboard-Keys-UI/index.html',
+        link: './OnlineTutotial/CSS/Keyboard-Keys-UI/index.html',
     },
     {
         name: 'Animated-Circular-Navigation-Menu',
-        link: '../OnlineTutotial/JS/Animated-Circular-Navigation-Menu/index.html',
+        link: './OnlineTutotial/JS/Animated-Circular-Navigation-Menu/index.html',
     },
     {
         name: 'Animated-Radial-Menu',
-        link: '../OnlineTutotial/JS/Animated-Radial-Menu/index.html',
+        link: './OnlineTutotial/JS/Animated-Radial-Menu/index.html',
     },
     {
         name: 'Live Character-Word Counter',
-        link: '../OnlineTutotial/JS/Live Character-Word Counter/index.html',
+        link: './OnlineTutotial/JS/Live Character-Word Counter/index.html',
     },
     {
         name: 'Magnetic-Hover-Effects',
-        link: '../OnlineTutotial/JS/Magnetic-Hover-Effects/index.html',
+        link: './OnlineTutotial/JS/Magnetic-Hover-Effects/index.html',
     },
     {
         name: 'Neumorphism-Custom-Range-Slider',
-        link: '../OnlineTutotial/JS/Neumorphism-Custom-Range-Slider/index.html',
+        link: './OnlineTutotial/JS/Neumorphism-Custom-Range-Slider/index.html',
     },
     {
         name: 'Range-Slider-Custom-Animated',
-        link: '../OnlineTutotial/JS/Range-Slider-Custom-Animated/index.html',
+        link: './OnlineTutotial/JS/Range-Slider-Custom-Animated/index.html',
     },
 
     {
@@ -42,35 +42,35 @@ const projects = [
     },
     {
         name: 'ProgressBar - File upload',
-        link: '../Progressbar/HoneyWebDesigner/index.html',
+        link: './Progressbar/HoneyWebDesigner/index.html',
     },
     {
         name: 'ProgressBar - File upload | http-bin',
-        link: '../Progressbar/OpenJavaScript/index.html',
+        link: './Progressbar/OpenJavaScript/index.html',
     },
     {
         name: 'Character Showcase',
-        link: '../CodingMonk/CSS/index.html',
+        link: './CodingMonk/CSS/index.html',
     },
     {
         name: 'Context Menu',
-        link: '../ContextMenu/CodingNepal/index.html',
+        link: './ContextMenu/CodingNepal/index.html',
     },
     {
         name: 'Hover Effect',
-        link: '../Hyperplexed/01.HoverEffect/index.html',
+        link: './Hyperplexed/01.HoverEffect/index.html',
     },
     {
         name: 'Layout-1',
-        link: '../LayoutSystem/Flex/layout-1/index.html',
+        link: './LayoutSystem/Flex/layout-1/index.html',
     },
     {
         name: 'Layout-2',
-        link: '../LayoutSystem/Flex/layout-2/index.html',
+        link: './LayoutSystem/Flex/layout-2/index.html',
     },
     {
         name: 'Layout-3',
-        link: '../LayoutSystem/Flex/layout-3/index.html',
+        link: './LayoutSystem/Flex/layout-3/index.html',
     }, 
     {
         name: 'Animate On Scroll',
