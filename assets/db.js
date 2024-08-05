d2dffddf908c2332e47a02885323a2eb09b3af25
taiseen <taiseen.cse@gmail.com>
2024-08-06 04:23:23 +0600

    refactor: all move inside project folder

--- assets/db.js
+++ assets/db.js
@@ -1,99 +1,99 @@
 const projects = [
     {
         name: 'Animated-Fancy-Border',
-        link: './OnlineTutotial/CSS/Animated-Fancy-Border/index.html',
+        link: './projects/OnlineTutorial/CSS/Animated-Fancy-Border/index.html',
     },
     {
         name: 'Keyboard-Keys-UI',
-        link: './OnlineTutotial/CSS/Keyboard-Keys-UI/index.html',
+        link: './projects/OnlineTutorial/CSS/Keyboard-Keys-UI/index.html',
     },
     {
         name: 'Animated-Circular-Navigation-Menu',
-        link: './OnlineTutotial/JS/Animated-Circular-Navigation-Menu/index.html',
+        link: './projects/OnlineTutorial/JS/Animated-Circular-Navigation-Menu/index.html',
     },
     {
         name: 'Animated-Radial-Menu',
-        link: './OnlineTutotial/JS/Animated-Radial-Menu/index.html',
+        link: './projects/OnlineTutorial/JS/Animated-Radial-Menu/index.html',
     },
     {
         name: 'Live Character-Word Counter',
-        link: './OnlineTutotial/JS/Live Character-Word Counter/index.html',
+        link: './projects/OnlineTutorial/JS/Live Character-Word Counter/index.html',
     },
     {
         name: 'Magnetic-Hover-Effects',
-        link: './OnlineTutotial/JS/Magnetic-Hover-Effects/index.html',
+        link: './projects/OnlineTutorial/JS/Magnetic-Hover-Effects/index.html',
     },
     {
         name: 'Neumorphism-Custom-Range-Slider',
-        link: './OnlineTutotial/JS/Neumorphism-Custom-Range-Slider/index.html',
+        link: './projects/OnlineTutorial/JS/Neumorphism-Custom-Range-Slider/index.html',
     },
     {
         name: 'Range-Slider-Custom-Animated',
-        link: './OnlineTutotial/JS/Range-Slider-Custom-Animated/index.html',
+        link: './projects/OnlineTutorial/JS/Range-Slider-Custom-Animated/index.html',
     },
     {
         name: 'SpinWheel Canvas',
-        link: './SpinWheel/codecc/index.html',
+        link: './projects/SpinWheel/codecc/index.html',
     },
     {
         name: 'SpinWheel',
-        link: './SpinWheel/OnlineTutorials/index.html',
+        link: './projects/SpinWheel/OnlineTutorials/index.html',
     },
     {
         name: 'ProgressBar - File upload',
-        link: './Progressbar/HoneyWebDesigner/index.html',
+        link: './projects/ProgressBar/HoneyWebDesigner/index.html',
     },
     {
         name: 'ProgressBar - File upload | http-bin',
-        link: './Progressbar/OpenJavaScript/index.html',
+        link: './projects/ProgressBar/OpenJavaScript/index.html',
     },
     {
         name: 'Character Showcase',
-        link: './CodingMonk/CSS/index.html',
+        link: './projects/CodingMonk/CSS/index.html',
     },
     {
         name: 'Context Menu',
-        link: './ContextMenu/CodingNepal/index.html',
+        link: './projects/ContextMenu/CodingNepal/index.html',
     },
     {
         name: 'Hover Effect',
-        link: './Hyperplexed/01.HoverEffect/index.html',
+        link: './projects/Hyperplexed/01.HoverEffect/index.html',
     },
     {
         name: 'Layout-1',
-        link: './LayoutSystem/Flex/layout-1/index.html',
+        link: './projects/LayoutSystem/Flex/layout-1/index.html',
     },
     {
         name: 'Layout-2',
-        link: './LayoutSystem/Flex/layout-2/index.html',
+        link: './projects/LayoutSystem/Flex/layout-2/index.html',
     },
     {
         name: 'Layout-3',
-        link: './LayoutSystem/Flex/layout-3/index.html',
+        link: './projects/LayoutSystem/Flex/layout-3/index.html',
     }, 
     {
         name: 'Animate On Scroll',
-        link: './SnippetsCode/AnimateOnScroll/index.html',
+        link: './projects/SnippetsCode/AnimateOnScroll/index.html',
     },
     {
         name: 'Multi Level Menu',
-        link: './SnippetsCode/MultiLevelMenu/index.html',
+        link: './projects/SnippetsCode/MultiLevelMenu/index.html',
     },
     {
         name: 'Table v1 - Product Table',
-        link: './ResponsiveTable/table-v1/index.html',
+        link: './projects/ResponsiveTable/table-v1/index.html',
     },
     {
         name: 'Table v2 - Student Table',
-        link: './ResponsiveTable/table-v2/index.html',
+        link: './projects/ResponsiveTable/table-v2/index.html',
     },
     {
         name: 'Table v3 - Student Table | Name Heading',
-        link: './ResponsiveTable/table-v3/index.html',
+        link: './projects/ResponsiveTable/table-v3/index.html',
     },
     {
         name: 'Table v4 - Student Table | Row Gap',
-        link: './ResponsiveTable/table-v4/index.html',
+        link: './projects/ResponsiveTable/table-v4/index.html',
     },
 ]
 
